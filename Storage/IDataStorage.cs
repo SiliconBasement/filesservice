29a0ddca42e77a477324750235da1863992d2b0f
Alex Kalinkin <alex@kalinkin.info>
2017-12-09 16:05:30 +0300

    Rebuild Interface to use async methods

--- Storage/IDataStorage.cs
+++ Storage/IDataStorage.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace ru.siliconbasement.micros.filesservice.storage {
     public interface IDataStorage
     {
-        System.Guid Save(string filePath);
-        object Load(System.Guid uid);
-
-        IEnumerable<object> LoadAll();
+        Task<Guid> SaveAsync(IFormFile file);
+        Task<Guid> SaveTempAsync(IFormFile file);
+        Task<object> GitFileInfoAsync(System.Guid uid);
 
         void Delete(System.Guid uid);
     }
