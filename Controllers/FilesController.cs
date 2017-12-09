643d5b56bd9703b6a254bdf3dfd84d598895e07b
Alex Kalinkin <alex@kalinkin.info>
2017-12-09 16:05:55 +0300

    Add some actions and FileUpload handler

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 using ru.siliconbasement.micros.filesservice.storage;
@@ -18,36 +22,82 @@ namespace ru.siliconbasement.micros.filesservice.controllers {
             _storage = storage;
         }       
 
-        // GET api/files
-        [HttpGet]
-        public IEnumerable<object> Get()
-        {
-            _logger.LogDebug("GET /api/files");
-            return _storage.LoadAll();
-        }
-
         // GET api/files/{uid}
         [HttpGet("{uid}", Name = "GetFile")]
-        public IActionResult Get(Guid uid)
+        public async Task<IActionResult> Get(Guid uid)
         {
             _logger.LogDebug("GET /api/files/{uid}", uid);
-            var item = _storage.Load(uid);
+            var item = await _storage.GitFileInfoAsync(uid);
             if (item == null)
             {
                 _logger.LogWarning("File not found. uid: {uid}", uid);
                 return NotFound();
             }
-            return new ObjectResult(item);
+            // TODO: Return file metadata {name, links { self, delete }}
+            return Ok(new { 
+                file = new { 
+                    name = "filename", 
+                    links = new {
+                        patch = @"http://fileservice/api/files/{uid}/download", 
+                        download = @"http://fileservice/api/files/{uid}/download", 
+                        delete = @"http://fileservice/api/files/{uid}/delete"
+                    }
+                }
+            });
         }
 
-        // // PUT api/files/5
-        // [HttpPut("{uid}")]
-        // public void Put(Guid uid, [FromBody]IUploadedFile value)
-        // {
-        // }
+        // POST api/files/upload
+        [HttpPost("upload")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            // long size = files.Sum(f => f.Length);
+            Guid[] uids = new Guid[files.Count];
+            for (var i=0; i < files.Count; i++)
+            {
+                if (files[i].Length > 0)
+                {
+                    uids[i] = await _storage.SaveAsync(files[i]);
+                }
+            }
 
-        // DELETE api/files/5
-        [HttpDelete("{uid}")]
+            return Ok(new { count = files.Count, uids = uids });
+        }
+
+        // POST api/files/preload
+        [HttpPost("preload")]
+        public async Task<IActionResult> PostPreload(List<IFormFile> files)
+        {
+            // long size = files.Sum(f => f.Length);
+            Guid[] uids = new Guid[files.Count];
+            for (var i=0; i < files.Count; i++)
+            {
+                if (files[i].Length > 0)
+                {
+                    uids[i] = await _storage.SaveTempAsync(files[i]);
+                }
+            }
+
+            return Ok(new { count = files.Count, uids = uids });
+        }
+
+// // see http://benfoster.io/blog/aspnet-core-json-patch-partial-api-updates
+//         // PATCH api/files/{uid}
+//         [HttpPatch("{uid}")]
+//         public async Task<IActionResult> Patch(Guid uid, [FromBody] IPartial<Person> partialPerson)
+//         {
+//             if (partialPerson.IsSet(p => p.Id))
+//             {
+//                 return BadRequest();
+//             }
+
+//             var person = await _person.Get(id);
+//             partialPerson.Populate(person);
+//             _person.Save(person);
+//             return Ok();
+//         }
+
+        // DELETE api/files/{uid}/delete
+        [HttpDelete("{uid}/delete")]
         public void Delete(Guid uid)
         {
             _storage.Delete(uid);
